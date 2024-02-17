766e32148a5bd49c5d8c5dcbd10e39edbcefe158
su-u <happy.oil2469@fastmail.com>
2024-02-17 15:41:11 +0000

    2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const get_logo_width = (): string => {
+const get_logo_width_2 = (): string => {
 	if (window.innerWidth >= 1920) return '100px';
 	if (
 		(window.innerWidth <= 992 && window.innerWidth >= 768) ||
